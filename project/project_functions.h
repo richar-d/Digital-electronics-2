dade927fa83f6b37d484c6eb50fd79bb5000e9b9
richar-d <60732239+richar-d@users.noreply.github.com>
2020-12-15 17:08:34 +0100

    updated comments

--- project/project_functions.h
+++ project/project_functions.h
@@ -9,10 +9,10 @@
 #ifndef PROJECT_FUNCTIONS_H_
 #define PROJECT_FUNCTIONS_H_
 
-void Display_dist(uint8_t id,volatile float dist[], char string[]);	//displays distance on lcd based on id
-void Update_warning(int sm_dist);								                  	//updates warning message on lcd
-void Load_bar(int distance);									                    	//updates load bar based on smaller distance
-void Uart_info(volatile float dist[]);						              		//displays info about closeness of obstacles on uart
+void Display_dist(uint8_t id,volatile float dist[], char string[]); //displays distance on lcd based on id
+void Update_warning(int sm_dist);                                   //updates warning message on lcd
+void Load_bar(int distance);                                        //updates load bar based on smaller distance
+void Uart_info(volatile float dist[]);                              //displays info about closeness of obstacles on uart
 
 
 #endif /* PROJECT_FUNCTIONS_H_ */
